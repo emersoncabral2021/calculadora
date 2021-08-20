f02228835c8235e5cdb5e772849c4797e7d7aace
emerson da conceição cabral <emersoncabral202@gmail.com>
2021-08-19 22:28:23 -0300

    funções

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -4,21 +4,17 @@ var repetioperacao = 0
 var selecionaroperacao = 0
 
 
-
-
-
-
 function digitenum(num){
     if(num == ' + '){
         selecionaroperacao = 1
     }else if(num == ' - '){
-        selecionaroperacao = 2
+         selecionaroperacao = 2
     }else if(num == ' x '){
-        selecionaroperacao = 3
+          selecionaroperacao = 3
     }else if(num == ' / '){
-        selecionaroperacao = 4
+         selecionaroperacao = 4
     }else if(num == ' % '){
-        selecionaroperacao = 5        
+         selecionaroperacao = 5        
     }
     
     if((num == ' + ') || (num == ' - ') || (num == ' x ') || (num == ' / ') || (num == ' % ') ){
@@ -30,7 +26,12 @@ function digitenum(num){
         }else if(repetioperacao == 1){
         pri.value = pri.value.substr(0,(pri.value.length)) + num
         }
-      } else if(num == ' = '){
+      }else if(num == ' = '){
+        igual(selecionaroperacao)
+      }else if (segnumero != 0){
+          limpar()
+      }
+       /*else if(num == ' = '){
         segnumero = Number(pri.value.substr((valor.length + 3),pri.value.length)) 
     if(selecionaroperacao == 1){
             resultado = prinumero + segnumero
@@ -54,7 +55,7 @@ function digitenum(num){
             repetioperacao = 0
         }
 
-    }/*else if(num == 'limpar'){
+    }else if(num == 'limpar'){
         pri.value = ""
         prinumero = ""
         segnumero = ""
@@ -78,6 +79,31 @@ function digitenum(num){
     pri.value += num
 
     }
+}
+
+function igual(selecionaroperacao){
+    segnumero = Number(pri.value.substr((valor.length + 3),pri.value.length)) 
+    if(selecionaroperacao == 1){
+            resultado = prinumero + segnumero
+            pri.value = resultado
+            repetioperacao = 0
+        }else if(selecionaroperacao == 2){
+            resultado = prinumero - segnumero
+            pri.value = resultado
+            repetioperacao = 0
+        }else if(selecionaroperacao == 3){
+            resultado = prinumero * segnumero
+            pri.value = resultado
+            repetioperacao = 0
+        }else if(selecionaroperacao == 4){
+            resultado = prinumero / segnumero
+            pri.value = resultado
+            repetioperacao = 0
+        }else if(selecionaroperacao == 5){
+            resultado = (prinumero / 100) * segnumero
+            pri.value = resultado
+            repetioperacao = 0
+        }
 }
 
 function apagar(){
